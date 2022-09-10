c249bcceb96b0402f1d3753924d9952825235f1a
jide-jide <threale90@gmail.com>
2022-09-10 09:50:42 -0700

    message

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - returns digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+	int n;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		for (n = 49; n <= 57; n++)
+		{
+			if (n > ch)
+			{
+				putchar(ch);
+
+				putchar(n);
+
+				if (ch != 56 || n != 57)
+				{
+					putchar(44);
+
+					putchar(32);
+				}
+			}
+		}
+	}
+
+	putchar(10); /* this is an ascii code for new line*/
+
+	return (0);
+}
