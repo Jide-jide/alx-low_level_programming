debbdc2930d93862e0d69c18f2090a603be15e1b
jide-jide <threale90@gmail.com>
2022-09-10 09:48:43 -0700

    message

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - returns digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+
+		if (ch != 57)
+		{
+			putchar(44);
+
+			putchar(32);
+		}
+	}
+
+	putchar(10); /* this is an ascii code for new line*/
+
+	return (0);
+}
+
