7623dc6a97d58693ace6e829ff347942731878e8
jide-jide <threale90@gmail.com>
2022-09-10 09:42:43 -0700

    message

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - returns letters
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+
+	putchar(10); /* this is an ascii code for new line*/
+
+	return (0);
+}
