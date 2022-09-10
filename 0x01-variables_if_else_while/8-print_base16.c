00dbd38f09dcb3189f3a02ef55acb0dfa56b5027
jide-jide <threale90@gmail.com>
2022-09-10 09:45:59 -0700

    message

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - returns digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar(10);
+
+	return (0);
+}
