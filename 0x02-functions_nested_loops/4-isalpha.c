e4bd778762af0005ba19450b102ff8d0f3ee8c18
jide-jide <threale90@gmail.com>
2022-09-13 07:13:31 -0700

    message

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*_islower(int c)-Write a function that checks for lowercase character.
+* @c : character to check the case
+*Return: Always 0 (Success)
+*/
+
+int _islower(int c)
+{
+	if (isalpha (c) || islower(c) || isupper(c))
+		return (1);
+	else
+		return (0);
+}
