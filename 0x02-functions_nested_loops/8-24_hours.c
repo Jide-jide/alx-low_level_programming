cf6288f63ce29217dd4e9076d1997fb869dfc1ab
jide-jide <threale90@gmail.com>
2022-09-13 11:34:34 -0700

    message

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * jack_bauer - prints the last digit of a number
+ * Return: integer value
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i=0; i < 24; i++)i
+	{
+		for (j=0; j <60; j++)
+			printf("%02d:%02d\n", i, j);
+	}
+}
+
