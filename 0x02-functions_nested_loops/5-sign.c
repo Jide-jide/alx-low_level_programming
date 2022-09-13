02513f221d35a964b3daf2c2ab043b3089ee7d1d
jide-jide <threale90@gmail.com>
2022-09-13 07:37:04 -0700

    message

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+*print_sign - Write a function that prints the sign of a number.
+* @n : character to check the case
+*Return: Always 0 (Success)
+*/
+
+int print_sign(int n)
+{
+
+        if (n > 0)
+        {
+            printf("+");
+            printf(",");
+            printf(" ");
+            return (1);
+        }
+        else if (n == 0)
+        {
+            printf("0");
+            printf(",");
+            printf(" ");
+            return (0);
+        }
+        else if (n < 0)
+        {
+            printf("-");
+            printf(",");
+            printf(" ");
+            return (-1);
+        }
+}
