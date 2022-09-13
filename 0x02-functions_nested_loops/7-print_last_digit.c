45c9c4e92f64fd2fb46672478f294f9a8f3782dc
jide-jide <threale90@gmail.com>
2022-09-13 12:21:19 -0700

    message

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,18 +3,28 @@
 
 /**
  * print_last_digit - return value
- * @x : the number to be checked
+ * @n : the number to be checked
  * Return:0 or 1
  */
 
+int print_last_digit(int n)
 
-
-int print_last_digit(int x)
 {
+
 	int last;
 
-	last = abs(x % 10);
-	_putchar(last);
 
-	return (0);
+
+	last = n % 10;
+
+
+
+	if (last < 0)
+
+	last = last * -1;
+
+	_putchar(last + '0');
+
+	return (last);
+
 }
