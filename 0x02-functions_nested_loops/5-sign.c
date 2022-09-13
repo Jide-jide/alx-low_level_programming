20645e4db1d5f2d3f03881966e23bbd029af3af2
jide-jide <threale90@gmail.com>
2022-09-13 07:47:17 -0700

    message

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,26 +8,25 @@
 
 int print_sign(int n)
 {
-
 	if (n > 0)
 	{
 		printf("+");
 		printf(",");
 		printf(" ");
-		return (1);
 	}
+	return (1);
 	else if (n == 0)
 	{
 		printf("0");
 		printf(",");
 		printf(" ");
-		return (0);
 	}
+	return (0);
 	else if (n < 0)
 	{
 		printf("-");
 		printf(",");
 		printf(" ");
-		return (-1);
 	}
+	return (-1);
 }
