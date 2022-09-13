4869bf7c8395c6b919b0bf7d816290bd51151892
jide-jide <threale90@gmail.com>
2022-09-13 08:19:20 -0700

    message

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - Write a function that computes the absolute value of an integer.
+ * @c : number to check
+ * Return:0 or 1
+ */
+
+int _abs(int c)
+{
+	int c;
+	c = abs(5.9);
+	_putchar(c)
+	return (0);
+}
