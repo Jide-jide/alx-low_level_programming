13430166922a71902a1051884850e7d071895d59
jide-jide <threale90@gmail.com>
2022-09-13 10:53:08 -0700

    message

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
 
-
+/**
+ * print_last_digit - return value
+ * @x : the number to be checked
+ * Return:0 or 1
+ */
 
 
 
 int print_last_digit(int x)
 {
 	int last;
+
 	last = x % 10;
+
 	return (last);
 }
