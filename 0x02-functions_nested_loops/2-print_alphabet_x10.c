701e5e7cdee6e0e530fc87511897e622ee3901cf
jide-jide <threale90@gmail.com>
2022-09-13 05:49:41 -0700

    message

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,8 +13,8 @@ void print_alphabet_x10(void)
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
-			_putchar("%c", j);
-			_putchar("\n");
+			_putchar('%c', j);
+			_putchar('\n');
 	}
 }
 
