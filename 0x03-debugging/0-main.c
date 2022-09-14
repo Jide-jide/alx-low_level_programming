5a7120510678b90c9afd990ac5dbde6d9525d623
jide-jide <threale90@gmail.com>
2022-09-14 12:49:45 -0700

    message

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-*main - testing positive_or_negative func
-*Return: Always 0 (Success)
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
 	int i;
 
 	i = 0;
-	void positive_or_negative(0);
-	
+	positive_or_negative(i);
+
 	return (0);
 }
