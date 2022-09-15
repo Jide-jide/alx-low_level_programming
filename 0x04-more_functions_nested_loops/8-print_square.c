fa2b4a3866aaec7c74df7b244e92a7e4f146609c
jide-jide <threale90@gmail.com>
2022-09-15 11:46:32 -0700

    message

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * print_square - prints square
+ * @size : size of square
+ * Return : void
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= size; x++)
+	{
+		for (y = 0; y <= size; y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}	
