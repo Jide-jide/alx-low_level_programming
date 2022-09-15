6a1cc20ce2ecfc90dec93d9e5fd63e56f26faa42
jide-jide <threale90@gmail.com>
2022-09-15 08:17:20 -0700

    message

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_isupper - checks if upper.
+ *@c : var to check
+ *
+ *Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	return (0);
+}
+
