8e35a59e0cc0d10649b8d5c192075824c05a7c09
jide-jide <threale90@gmail.com>
2022-09-15 09:24:19 -0700

    message

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
+
+
