f491edd4d8cccca8243d4267d966666606f33350
jide-jide <threale90@gmail.com>
2022-09-15 09:16:12 -0700

    message

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul(int a, int b) - multuplies 2 nums
+ * @a : num 1
+ * @b : num 2
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
