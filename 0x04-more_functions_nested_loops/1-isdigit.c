03726d5a93a664ca8232c90e9bca4b80cbee05b7
jide-jide <threale90@gmail.com>
2022-09-15 08:51:59 -0700

    message

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the character is digit
+ * @c: is the int used for the argument of the function.
+ * Return: 1 if digit else 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if (c >=0 && c <=9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
