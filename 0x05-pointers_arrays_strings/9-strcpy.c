376019dc84307edca52e38bb15b3122c532e699e
jide-jide <threale90@gmail.com>
2022-09-19 08:56:51 -0700

    message

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,27 +2,27 @@
 
 /**
  *_strcpy - update value.
- *@dest : value to be evaluate.
- *@src : value to be evaluate.
- *Return : Always 0 (success)
+ *@dest: value to be evaluate.
+ *@src: value to be evaluate.
+ *Return: Always 0.
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int y = 0;
+	int l = 0;
 	int x = 0;
 
-	while (*(src + y) != '\0')
+	while (*(src + l) != '\0')
 	{
-		y++;
+		l++;
 	}
 
-	for (; x < y; x++)
+	for (; x < l; x++)
 	{
 		dest[x] = src[x];
 	}
 
-	dest[y] = '\0';
+	dest[l] = '\0';
 
 	return (dest);
 }
