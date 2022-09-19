c83d27838dff9b9ee87024e6b31e68caac0481a7
jide-jide <threale90@gmail.com>
2022-09-19 07:56:48 -0700

    message

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - puts every other two
+ * @str : str argument
+ * Return : void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i < strlen(str))
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
