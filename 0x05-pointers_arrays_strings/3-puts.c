9a07a8eea91019d3e10f38084e90a44d7e9016dd
jide-jide <threale90@gmail.com>
2022-09-19 06:19:00 -0700

    message

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - prints a string
+ * @str : char type 
+ * Return : void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i < strlen(str); i++)
+	       putchar(s[i]);
+}	
