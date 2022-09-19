e0007a595ec81d062bab7a21f3fbfdc678a67a1f
jide-jide <threale90@gmail.com>
2022-09-19 07:14:30 -0700

    message

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - prints rev of a string
+ * @s : char type
+ * Return : void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+		_putchar(s[i]);
+}
