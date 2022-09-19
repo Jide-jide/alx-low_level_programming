033aa513c82661f63f55efe612db8c04c79e7107
jide-jide <threale90@gmail.com>
2022-09-19 05:51:25 -0700

    message

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - length of  a string
+ * @s : char type
+ * Return ; Always 0 (success)
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
