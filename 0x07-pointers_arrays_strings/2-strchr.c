63e98c5d66420011986e7328b5311cab408ce3a3
jide-jide <threale90@gmail.com>
2022-09-26 12:45:38 -0700

    message

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,29 @@
 #include "main.h"
 #include <string.h>
 
+/**
+  * _strchr - locate character in string
+  * @s: source string
+  * @c: character to find
+  *
+  * Return: the string from character found
+  */
+
 char *_strchr(char *s, char c)
 {
 	unsigned int i;
-	char *p;
+	int *p;
 
 	for (i = 0; i <= strlen(s); i++)
 	{
 		if (s[i] == c)
 		{
-			p = s[i];
+			p = *s[i];
 
 			return (p);
 			break;
 		}
 		else
-		{
 			return (0);
-		}
 	}
 }
