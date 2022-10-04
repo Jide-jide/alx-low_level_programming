24e5c4fdb1da4014f6d5a1c6d6fcfac0ce92ec2e
jide-jide <threale90@gmail.com>
2022-10-04 04:31:18 -0700

    message

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
 	char *p = NULL;
 
 	if (size == 0)
@@ -30,4 +30,5 @@ char *create_array(unsigned int size, char c)
 			}
 		}
 	}
+	return (p);
 }
